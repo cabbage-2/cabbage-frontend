6de98b6daad4db1d97abd91c1c826ba0d2ff826a
k11n11 <k11n11@users.noreply.github.com>
2022-07-10 06:01:21 +0800

    Merge?

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,22 +1,27 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Header from "../Components/Header";
 import StoreSearch from "../Components/StoreSearch";
 import StoreSearchBar from "../Components/StoreSearchBar";
 import StoreSearchResults from "../Components/StoreSearchResults";
 import styles from "./Home.module.scss";
+import {StoreContext} from "../Components/storecontext";
+import PreMeal from "../Components/portions";
 
 const Home = ({ currentUser, signOut }) => {
-  const [searchResults, setSearchResults] = useState(["Fooding", "Makan"]);
+  const [sel,setSel] = useState("makan")
   return (
-    <div className={styles["container"]}>
-      <div className={styles["content--wrapper"]}>
-        <Header signOut={signOut} />
-        <h3 className={styles["content--title"]}>
-          Welcome back, {currentUser.displayName}!
-        </h3>
-        <StoreSearch />
+    <StoreContext.Provider value={{sel, setSel}}>
+      <div className={styles["container"]}>
+        <div className={styles["content--wrapper"]}>
+          <Header signOut={signOut} />
+          <h3 className={styles["content--title"]}>
+            Welcome back, {currentUser.displayName} {sel}!
+          </h3>
+          <StoreSearch />
+        </div>
+      <PreMeal store={sel} userweight={1}/>
       </div>
-    </div>
+    </StoreContext.Provider>
   );
 };
 
